0709a9d958f7eb5c89c248fd00fecb866869c6b0
Source-404 <suyashjohri3@gmail.com>
2021-09-27 10:23:25 +0530

    removed clutter

--- src/index.js
+++ src/index.js
@@ -5,56 +5,13 @@ const bucketRouter = require("./routers/bucket");
 const path = require("path");
 const hbs = require("hbs");
 
-// paths for config
-const publicDirectoryPath = path.join(__dirname, "..");
-const viewPath = path.join(__dirname, "../templates/views");
-const partials = path.join(__dirname, "../templates/partials");
-
 const app = express();
 const port = process.env.PORT;
-
-// handle bars setup
-app.set("view engine", "hbs");
-app.set("views", viewPath);
-hbs.registerPartials(partials);
 
 //seperate routers
 app.use(express.json());
 app.use(userRouter);
 app.use(bucketRouter);
-
-//setup static directory to serve
-app.use(express.static(publicDirectoryPath));
-
-app.get("", (req, res) => {
-  res.render("index");
-});
-
-//routes for front-end
-
-app.get("/sign-up", (req, res) => {
-  res.render("sign-up", {
-    title: "Sign-up",
-  });
-});
-
-app.get("/gallery", (req, res) => {
-  res.render("gallery");
-});
-
-app.get("/add", (req, res) => {
-  res.render("add");
-});
-app.get("/profile", (req, res) => {
-  res.render("profile");
-});
-// app.post("/welcome", (req, res) => {
-//   // const name = req.query.name;
-//   res.render("welcome", {
-//     title: "Welcome",
-//     name: "source",
-//   });
-// });
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
